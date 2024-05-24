db3a237fed47fabe04aae084d7d27300057ee779
vyshnavichidire <vyshnavichidire@gmail.com>
2024-05-24 16:52:37 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import About from "./Components/About";
+import Navbar from "./Components/Navbar";
+import Projects from "./Components/Projects";
+import Contact from "./Components/Contact";
+import Testimonials from "./Components/Testimonials";
+
+export default function App() {
+  return (
+    <main>
+      <Navbar />
+      <About />
+      <Projects />
+      <Skills />
+      <Testimonials />
+      <Contact />
+    </main>
+  );
+}
